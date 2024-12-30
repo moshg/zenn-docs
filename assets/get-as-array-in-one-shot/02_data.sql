c5afe6a341b6352599c1837c60839820b06ad7b5
mosh <mosh.ohayo@gmail.com>
2024-12-30 17:38:58 +0900

    動作確認環境追加

---
+++ assets/get-as-array-in-one-shot/02_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+    groups (name)
+VALUES
+    ('group_1'),
+    ('group_2');
+
+INSERT INTO
+    items (group_id, name)
+VALUES
+    (1, 'item_1'),
+    (1, 'item_2'),
+    (2, 'item_3');
